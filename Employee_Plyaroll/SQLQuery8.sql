d66afcdbd0abdd5ac1e2de2c46b81b82e6cd8d78
Akshata72 <99897062+Akshata72@users.noreply.github.com>
2022-04-25 20:13:04 +0530

    Update SQLQuery8.sql

--- Employee_Plyaroll/SQLQuery8.sql
+++ Employee_Plyaroll/SQLQuery8.sql
@@ -4,9 +4,10 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROCEDURE[ReturnAllRecords()]
+CREATE PROCEDURE[GetAllEmployeePayrollData()]
 AS
 BEGIN
-SELECT Employee_Payroll.ID,First_Name,Last_Name,Gender,Basic_Pay,Deducations,Taxable_Pay,Income_Tax,Net_Pay from Employee_Payroll
+SELECT Employee_Payroll.ID,First_Name,Last_Name,Gender,Basic_Pay,Deducations,Taxable_Pay,Income_Tax,Net_Pay from Employee_Payroll 
 END
 GO
+exec [GetAllEmployeePayrollData()];
